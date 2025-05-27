f6e2e14037d369ad964ab0b7fb712079b8f23c25
DerekFranca <derekfrancatf@gmail.com>
2025-05-26 20:46:48 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -5,15 +5,20 @@ export default function Index() {
     <View>
       <Text className="bg-blue-500 text-white pl-3 h-10 text-2xl">Detalhe do produto</Text>
 
-    <View>
+    <View className="flex-1 justify-center items-center">
       <Image 
-      className="w-32 h-32 rounded-full"
+      className="w-64 h-64 rounded-full items-center"
       source={{uri: 'https://static.vecteezy.com/system/resources/previews/022/996/364/non_2x/free-golden-chinese-dragon-ai-generative-free-png.png'}}/>
   
     </View>
-
-  
-
+    <View>
+    <Text className="pl-3 font-bold pt-9 h-10 text-2xl">Detalhe do produto</Text>
+    <Text className="pl-3 pt-9 h-10">Este produto foi criado com materiais de alta qualidade, visando sempre o conforto e a durabilidade. É perfeito para quem busca praticidade e estilo</Text>
+    <Text className="color-green-400 pl-3 font-bold pt-[70px] h-10 text-xl ">R$ 299,99</Text>
+    </View>
+      <TouchableOpacity className="bg-blue-500 py-2 px-4 rounded-full mt-[40px]">
+        <Text className="text-center color-white font-bold text-xl">click</Text>
+      </TouchableOpacity>
     </View>
   );
 }
